d61d30d09a20c8e23cbe11120a88c1479ca576c8
Eduardo Cortés Herrera <eduardocortes3373@gmail.com>
2025-03-31 11:41:04 -0600

    Update sort param from employee feature

--- src/features/employee/query.js
+++ src/features/employee/query.js
@@ -3,6 +3,7 @@ import { helpers } from '../../helpers/index.js';
 const index = helpers.query.index({
   search: ['code', 'name'],
   filters: ['is_active', 'department_id'],
+  sort: ['code', 'name', 'is_active', 'department_id'],
 });
 
 export const query = {
